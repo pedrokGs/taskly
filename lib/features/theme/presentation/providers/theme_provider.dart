9fa688c892ccb34bdf6b9ea5a5fb1b9c7bca49f8
Pedro Henrique <pedibr5@gmail.com>
2025-10-08 19:57:10 -0300

    Removido dependência get_it, agora a injeção é feita por meio do riverpod

--- lib/features/theme/presentation/providers/theme_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:taskly/core/get_it/get_it.dart';
-import 'package:taskly/features/theme/presentation/providers/theme_notifier.dart';
-
-final themeNotifierProvider = AsyncNotifierProvider<ThemeNotifier, ThemeMode>(
-      () => getIt<ThemeNotifier>(),
-);
